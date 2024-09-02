da66672ae952aa28e24379c090c1e1a53570020f
maryam aalizadeh <maalizadeh7394@gmail.com>
2024-09-02 15:01:49 +0430

    jWT authentication

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { signUp, signin, getAllUsers } = require("../controllers/user");
+const { verifyToken } = require("../config/isAuth");
+const router = express.Router();
+
+router.post("/signup", signUp);
+router.post("/signin", signin);
+router.get("/allUsers", verifyToken, getAllUsers);
+
+module.exports = router;
